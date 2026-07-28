60dc2cc32d66f182af902c8d2292eede030c609e
Joshit-innit <tammanajoshit2006@gmail.com>
2026-07-28 18:41:21 +0530

    Added Swagger and Swagger UI

--- src/main/java/com/klu/soa/controller/DoctorController.java
+++ src/main/java/com/klu/soa/controller/DoctorController.java
@@ -3,16 +3,23 @@ package com.klu.soa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.klu.soa.entity.Doctor;
 import com.klu.soa.service.DoctorService;
 
 @RestController
+@RequestMapping("/doctor")
 public class DoctorController {
 	
 	@Autowired
@@ -36,6 +43,52 @@ public class DoctorController {
 		List<Doctor> doctors = service.displayAllDoctors();
 		return ResponseEntity.status(200).body(doctors);
 	}
+	
+	// Display docotor by id
+	
+	@GetMapping("/display")
+	public ResponseEntity<?> displayDoctorById(@RequestParam Long id) {
+		
+		Doctor doctor = service.displayDoctorById(id);
+		
+		if (doctor != null) {
+			return ResponseEntity.status(200).body(doctor);
+		}
+		return ResponseEntity.status(404).body("Doctor not there");
+		
+	}
+	
+	// Update Doctor
+		@PutMapping("/update")
+		public ResponseEntity<?> updateDoctor(@RequestBody Doctor doctor)
+		{
+			Doctor d = service.updateDoctor(doctor);
+
+			if (d != null)
+			{
+				return ResponseEntity.ok(d);
+			}
+			else
+			{
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Doctor ID Not Found");
+			}
+		}
+
+		// Delete Doctor By ID
+		@DeleteMapping("/delete/{id}")
+		public ResponseEntity<String> deleteDoctorById(@PathVariable Long id)
+		{
+			String message = service.deleteDoctorById(id);
+			return new ResponseEntity<>(message, HttpStatus.OK);
+		}
+
+		// Display Doctors By Gender
+		@GetMapping("/displaybygender/{gender}")
+		public ResponseEntity<List<Doctor>> displayDoctorsByGender(@PathVariable String gender)
+		{
+			List<Doctor> doctors = service.displayDoctorsByGender(gender);
+			return ResponseEntity.status(200).body(doctors);
+		}
 	
 	
 }
